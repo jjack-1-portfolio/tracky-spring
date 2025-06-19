fb405aaf2d8c49a7e680be1f4855c1094acecc24
Sxias <a32176740@gmail.com>
2025-06-19 22:11:38 +0900

    start

---
+++ src/main/java/com/example/tracky/TrackyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackyApplication.class, args);
+	}
+
+}
